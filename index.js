4d82c3fe42eec49b0e67887586b958daee7ab316
Mary Novosad <marichka.novosad@gmail.com>
2019-05-14 01:35:48 +0300

    Add integration with AWS S3 storage using aws-sdk package Add simple report which gets info about employee sales by country and saves it info to S3 storage as json file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var sql = require("mssql");
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
 
 var config = {
     user: 'nodejsteam2',
@@ -10,10 +12,26 @@ var config = {
 exports.handler = async (event, context, callback) => {
     try {
         let pool = await sql.connect(config);
-        let orders = await pool.request()
-            .query('select * from Orders');
+        let employeeSalesByCountry = (await pool.request()
+            .query("select distinct b.*, a.CategoryName from Categories a"
+            + " inner join Products b on a.CategoryID = b.CategoryID "
+            + "where b.Discontinued = 0 order by b.ProductName;")).recordset;
+
+        var bucketName = process.env.bucketName;
+        var date = new Date().toJSON().slice(0,10).replace(/-/g,'_');
+        var keyName = "jsonReports/report_" + date + ".json";
+        var content = JSON.stringify(employeeSalesByCountry);
+    
+        var params = { Bucket: bucketName, Key: keyName, Body: content };
+    
+        s3.putObject(params, function (err, data) {
+            if (err)
+                console.log(err)
+            else
+                console.log("Successfully saved object to " + bucketName + "/" + keyName);
+        });
             
-        console.log(orders);
+        console.log(employeeSalesByCountry);
         callback(null, "success!");
     } catch (err) {
         console.log("error!");
