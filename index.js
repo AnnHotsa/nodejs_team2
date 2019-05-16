2ee0b6607408740cf0e9d950e0ebf2554f4bae13
DawnSpark <uageneral@gmail.com>
2019-05-16 14:27:49 +0300

    Fixed SQL insert

--- index.js
+++ index.js
@@ -34,61 +34,44 @@ exports.handler = async (event, context, callback) => {
             WHERE O.OrderDate BETWEEN '${dateToSQL(fromDate)}' AND '${dateToSQL(toDate)}'
             GROUP BY O.ShipCountry, E.EmployeeID`;
 
-        let employeeSalesByCountry;
-        let reportMetadata;
-        await sql.connect(config)
-            .then((conn) => conn.query(query, function (err, recordset) {
-                if (err) callback(err);
-                    employeeSalesByCountry = recordset;
-            })
-            .then(() => {
-                //S3
-                var bucketName = process.env.bucketName;
-                var date = new Date().toJSON().slice(0,10).replace(/-/g,'_');
-                var keyName = "jsonReports/report_" + date + ".json";
-                var content = JSON.stringify(employeeSalesByCountry);
-            
-                var params = { Bucket: bucketName, Key: keyName, Body: content };
-            
-                s3.putObject(params, function (err, data) {
-                    if (err)
-                        console.log(err)
-                    else
-                        console.log("Successfully saved object to " + bucketName + "/" + keyName);
-                });
+        let pool = await sql.connect(config);
+        let employeeSalesByCountry = await pool.request()
+            .query(query);
+        //S3
+        var bucketName = process.env.bucketName;
+        var date = new Date().toJSON().slice(0,10).replace(/-/g,'_');
+        var keyName = "jsonReports/report_" + date + ".json";
+        var content = JSON.stringify(employeeSalesByCountry);
+
+        var params = { Bucket: bucketName, Key: keyName, Body: content };
+
+        s3.putObject(params, function (err, data) {
+            if (err)
+                console.log(err)
+            else
+                console.log("Successfully saved object to " + bucketName + "/" + keyName);
+        });
                 
-                //Write to SQL
-                    reportMetadata = {
+        let   reportMetadata = {
                         S3Location: `https://s3-eu-central-1.amazonaws.com/${bucketName}/${keyName}`,
                         CreatedDate: new Date(),
                         ReportFrom: fromDate,
                         ReportTo: toDate
                     }
-
-            console.log(employeeSalesByCountry);
-            //callback(null, "Success!");
-
-            })
-            .then(() => {
-                try {
-                    let metaquery = `INSERT INTO ReportsMetadata (S3Location, ReportFrom, ReportTo, CreatedDate)
-                    VALUES (${reportMetadata.S3Location}, '${dateToSQL(reportMetadata.ReportFrom)}', '${dateToSQL(reportMetadata.ReportTo)}', '${dateToSQL(reportMetadata.CreatedDate)}')`;
-                    conn.query(metaquery)
-                         .then((res) => console.log(`Metadata inserted! ${res} rows affected`))
-                         .then(() => conn.close())
-                         .then(sql.close());
-                }
-                catch (err) {
-                    console.log("error!");
-                    console.log(err);
-                    callback(err);
-                }
-            })
-            .then(() => conn.close())
-             ).then(callback(null, "Success!"));
-            
+        console.log(reportMetadata);
+        let metaquery = `INSERT INTO ReportsMetadata (S3Location, ReportFrom, ReportTo, CreatedDate)
+                    VALUES ('${reportMetadata.S3Location}', '${dateToSQL(reportMetadata.ReportFrom)}', '${dateToSQL(reportMetadata.ReportTo)}', '${dateToSQL(reportMetadata.CreatedDate)}')`;
         
-    } catch (err) {
+        let reportMetadatares = await pool.request()
+            .query(metaquery);
+
+        console.log(` Added ${reportMetadatares.rowsAffected} new metadata rows in SQL`);
+        console.log(employeeSalesByCountry);
+        pool.close();
+        
+        callback(null, "Success!");
+    }
+     catch (err) {
         console.log("error!");
         console.log(err);
         callback(err);
